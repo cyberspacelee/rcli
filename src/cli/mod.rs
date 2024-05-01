168e4864aa92b263721ee393a20a9791795bcb58
cyberspacelee <xingcanli132@163.com>
2024-05-01 19:11:03 +0800

    feature: add base64

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,11 +1,14 @@
+mod base64;
 mod csv;
 mod genpass;
 
+use std::path::Path;
+
+pub use base64::{Base64Format, Base64SubCommand};
 use clap::Parser;
 use csv::CsvOpts;
-use genpass::GenPassOpts;
-
 pub use csv::OutputFormat;
+use genpass::GenPassOpts;
 
 #[derive(Debug, Parser)]
 #[command(name = "rcli", version, author, about, long_about = None)]
@@ -21,12 +24,29 @@ pub enum SubCommand {
 
     #[command(name = "genpass", about = "Generate a random password")]
     GENPASS(GenPassOpts),
+
+    #[command(subcommand)]
+    BASE64(Base64SubCommand),
 }
 
-fn verify_input_file(file_name: &str) -> Result<String, String> {
-    if std::path::Path::new(file_name).exists() {
-        Ok(file_name.to_string())
+fn verify_input_file(filename: &str) -> Result<String, &'static str> {
+    // if input is "-" or file exists
+    if filename == "-" || Path::new(filename).exists() {
+        Ok(filename.into())
     } else {
-        Err(format!("File not found: {}", file_name))
+        Err("File does not exist")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_verify_input_file() {
+        assert_eq!(verify_input_file("-"), Ok("-".into()));
+        assert_eq!(verify_input_file("*"), Err("File does not exist"));
+        assert_eq!(verify_input_file("Cargo.toml"), Ok("Cargo.toml".into()));
+        assert_eq!(verify_input_file("not-exist"), Err("File does not exist"));
     }
 }
